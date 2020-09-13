660da1a7c618217b4a34a01cb23b755a575b05e2
YunMin-Song <thd930308@naver.com>
2020-09-13 23:47:18 +0900

    Update

--- Project1/HW1.c
+++ Project1/HW1.c
@@ -13,70 +13,59 @@ typedef struct node {
 	struct node* next;
 } node;
 
+void bubbleSort(node* start)
+{
+	int changed, i;
+	node* ptr;
+	node* lptr = NULL;
+
+
+}
+
 int main(void)
 {
+	// 필요한 포인터 및 변수 선언
 	FILE* fp;
 	element data;
 	node* head = NULL;
-	node* p, pp;
+	node* p;
 	fp = fopen("data.txt", "rt");
-	int sn, sc, i, j, count = 0;
+	int sn, sc, i, j, count = 0, status = 1;
 	char sname[20];
+	int changed = 0;
 
-
+	// 파일 예외처리
 	if (fp == NULL) {
 		printf("File Not Found\n");
 		exit(-1);
 	}
+
 	// 파일 읽고 Node 만듬
 	while (!feof(fp)) {
-		node* ptr = head;
-		node* p;
 		node* newnode = (node*)malloc(sizeof(node));
+		// 파일 입력
 		fscanf(fp, "%d %s %d", &sn, &sname, &sc);
+		// Node data에 File value 대입
 		newnode->data.studentNum = sn;
 		strcpy(newnode->data.studentName, sname);
 		newnode->data.studentScore = sc;
-		newnode->next = NULL;
 
-		if (head == NULL) {
-			(head) = newnode;
-			newnode->next = NULL;
-		}
-		else {
-			while (ptr != NULL) {
-				p = ptr;
-				ptr = ptr->next;
-			}
-			ptr = newnode;
-			p->next = ptr;
-		}
-		count++;
+		newnode->next = head;
+		head = newnode;
 	}
 
-	node* buf = head;
-	// 학번순 정렬이요
-	for (p=head; p->next != NULL; p=p->next) {
-		if (buf->data.studentNum > buf->next->data.studentNum) {
-			int tempNum = buf->next->data.studentNum;
-			buf->next->data.studentNum = buf->data.studentNum;
-			buf->data.studentNum = tempNum;
-
-			char tempName[20];
-			strcpy(tempName, buf->next->data.studentName);
-			strcpy(buf->next->data.studentName, buf->data.studentName);
-			strcpy(buf->data.studentName, tempName);
-
-			int tempScore = buf->next->data.studentScore;
-			buf->next->data.studentScore = buf->data.studentScore;
-			buf->data.studentScore = tempScore;
-		}
+	if (head == NULL) {
+		printf("Node Not Found!!\n");
+		return;
 	}
+
+
+
 
 	// 출력 하는 놈
-	p = head;
 	printf("학번순이요\n");
 
+	p = head;
 	while (p != NULL) {
 		printf("학번 : %d\n", p->data.studentNum);
 		printf("이름 : %s\n", p->data.studentName);
@@ -85,76 +74,8 @@ int main(void)
 	}
 	printf("\n\n");
 
-	//buf = head;
-	//// 이름순 정렬이요
-	//for (i = 0; i < count; i++) {
-	//	for (j = 0; j < count - 1; j++) {
-	//		if (strcmp(buf->data.studentName, buf->next->data.studentName) == 1) {
-	//			int tempNum = buf->next->data.studentNum;
-	//			buf->next->data.studentNum = buf->data.studentNum;
-	//			buf->data.studentNum = tempNum;
-
-	//			char tempName[20];
-	//			strcpy(tempName, buf->next->data.studentName);
-	//			strcpy(buf->next->data.studentName, buf->data.studentName);
-	//			strcpy(buf->data.studentName, tempName);
-
-	//			int tempScore = buf->next->data.studentScore;
-	//			buf->next->data.studentScore = buf->data.studentScore;
-	//			buf->data.studentScore = tempScore;
-
-	//		}
-	//	}
-	//}
-
-	//// 출력 하는 놈
-	//p = head;
-	//printf("이름순이요\n");
-
-	//while (p != NULL) {
-	//	printf("학번 : %d\n", p->data.studentNum);
-	//	printf("이름 : %s\n", p->data.studentName);
-	//	printf("총점 : %d\n", p->data.studentScore);
-	//	p = p->next;
-	//}
 	//printf("\n\n");
 
-	//buf = head;
-	//// 총점순 정렬이요
-	//for (i = 0; i < count; i++) {
-	//	for (j = 0; j < count - 1; j++) {
-	//		if (buf->data.studentScore < buf->next->data.studentScore) {
-	//			int tempNum = buf->next->data.studentNum;
-	//			buf->next->data.studentNum = buf->data.studentNum;
-	//			buf->data.studentNum = tempNum;
-
-	//			char tempName[20];
-	//			strcpy(tempName, buf->next->data.studentName);
-	//			strcpy(buf->next->data.studentName, buf->data.studentName);
-	//			strcpy(buf->data.studentName, tempName);
-
-	//			int tempScore = buf->next->data.studentScore;
-	//			buf->next->data.studentScore = buf->data.studentScore;
-	//			buf->data.studentScore = tempScore;
-
-	//		}
-	//	}
-	//}
-
-	//// 출력 하는 놈
-	//p = head;
-	//printf("총점순이요\n");
-
-
-	//while (p != NULL) {
-	//	printf("학번 : %d\n", p->data.studentNum);
-	//	printf("이름 : %s\n", p->data.studentName);
-	//	printf("총점 : %d\n", p->data.studentScore);
-	//	p = p->next;
-	//}
-
-	//printf("\n\n");
-
-
+	fclose(fp);
 	return 0;
 }
