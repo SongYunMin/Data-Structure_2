90c86e77f29fc0223a4f57923fc7ac88be62e0db
Song-YunMin <48168717+SongYunMin@users.noreply.github.com>
2020-09-14 16:17:41 +0900

    Update

--- Project1/HW1.c
+++ Project1/HW1.c
@@ -13,6 +13,63 @@ typedef struct node {
 	struct node* next;
 } node;
 
+
+// 학번 기준 정렬
+node* sortNumber(node* start)
+{
+	node* i = NULL;
+	node* j = NULL;
+	
+	for (i = start; i->next != NULL; i = i->next) {
+		for (j = start; j->next != NULL; j = j->next) {
+			if (j->data.studentNum > j->next->data.studentNum) {
+				element temp = j->data;
+				j->data = j->next->data;
+				j->next->data = temp;
+			}
+		}
+	}
+	return start;
+}
+
+// 이름 기준 정렬
+node* sortName(node* start)
+{
+	node* i = NULL;
+	node* j = NULL;
+
+	for (i = start; i->next != NULL; i = i->next) {
+		for (j = start; j->next != NULL; j = j->next) {
+			if (strcmp(j->data.studentName,j->next->data.studentName) == 1) {
+				element temp = j->data;
+				j->data = j->next->data;
+				j->next->data = temp;
+			}
+		}
+	}
+	return start;
+}
+
+// 총점 기준 정렬
+node* sortScore(node* start)
+{
+	node* i = NULL;
+	node* j = NULL;
+
+	for (i = start; i->next != NULL; i = i->next) {
+		for (j = start; j->next != NULL; j = j->next) {
+			if (j->data.studentScore < j->next->data.studentScore) {
+				element temp = j->data;
+				j->data = j->next->data;
+				j->next->data = temp;
+			}
+		}
+	}
+	return start;
+}
+
+
+
 int main(void)
 {
 	// 필요한 포인터 및 변수 선언
@@ -50,14 +107,9 @@ int main(void)
 		return;
 	}
 
-	/*
-	Sort Area
-	*/
-
-
-	// 출력 하는 놈
+	// 학번순
 	printf("학번순이요\n");
-
+	head = sortNumber(head);
 	p = head;
 	while (p != NULL) {
 		printf("학번 : %d\n", p->data.studentNum);
@@ -67,7 +119,30 @@ int main(void)
 	}
 	printf("\n\n");
 
-	//printf("\n\n");
+
+	// 이름(가나다)순
+	printf("이름순이요\n");
+	head = sortName(head);
+	p = head;
+	while (p != NULL) {
+		printf("학번 : %d\n", p->data.studentNum);
+		printf("이름 : %s\n", p->data.studentName);
+		printf("총점 : %d\n", p->data.studentScore);
+		p = p->next;
+	}
+	printf("\n\n");
+
+	// 총점순
+	printf("총점순이요\n");
+	head = sortScore(head);
+	p = head;
+	while (p != NULL) {
+		printf("학번 : %d\n", p->data.studentNum);
+		printf("이름 : %s\n", p->data.studentName);
+		printf("총점 : %d\n", p->data.studentScore);
+		p = p->next;
+	}
+	printf("\n\n");
 
 	fclose(fp);
 	return 0;
--- Project1/test.c
+++ Project1/test.c
@@ -1,102 +1,20 @@
-//// C program to implement Bubble Sort on singly linked list
-//#include<stdio.h> 
-//#include<stdlib.h> 
+//#include  <stdio.h>
 //
-///* structure for a node */
-//struct Node
+//
+//void print(int a)
 //{
-//    int data;
-//    struct Node* next;
-//};
-//
-//void insertAtTheBegin(struct Node** start_ref, int data);
-//void bubbleSort(struct Node* start);
-//void swap(struct Node* a, struct Node* b);
-//void printList(struct Node* start);
-//
-//int main()
-//{
-//    int arr[] = { 12, 56, 2, 11, 1, 90 };
-//    int list_size, i;
-//
-//    /* start with empty linked list */
-//    struct Node* start = NULL;
-//
-//
-//    // 데이터가 들어갈 공간을 할당받고 노드를 생성함
-//    for (i = 0; i < 6; i++)
-//        insertAtTheBegin(&start, arr[i]);
-//
-//    /* print list before sorting */
-//    printf("\n Linked list before sorting ");
-//    printList(start);
-//
-//    /* sort the linked list */
-//    bubbleSort(start);
-//
-//    /* print list after sorting */
-//    printf("\n Linked list after sorting ");
-//    printList(start);
-//
-//    getchar();
-//    return 0;
+//	if (a == 1)
+//		printf("true");
+//	else
+//		printf("false");
 //}
-//
-//
-///* Function to insert a node at the beginning of a linked list */
-//void insertAtTheBegin(struct Node** start_ref, int data)
+//int main(void)
 //{
-//    struct Node* ptr1 = (struct Node*)malloc(sizeof(struct Node));
-//    ptr1->data = data;
-//    ptr1->next = *start_ref;
-//    *start_ref = ptr1;
-//}
+//	int a = 5, b = 3;
 //
-///* Function to print nodes in a given linked list */
-//void printList(struct Node* start)
-//{
-//    struct Node* temp = start;
-//    printf("\n");
-//    while (temp != NULL)
-//    {
-//        printf("%d ", temp->data);
-//        temp = temp->next;
-//    }
-//}
+//	
+//	print(a>b ? 1 : 0);
 //
-///* Bubble sort the given linked list */
-//void bubbleSort(struct Node* start)
-//{
-//    int swapped, i;
-//    struct Node* ptr1;
-//    struct Node* lptr = NULL;
 //
-//    /* Checking for empty list */
-//    if (start == NULL)
-//        return;
-//
-//    do
-//    {
-//        swapped = 0;
-//        ptr1 = start;
-//
-//        while (ptr1->next != lptr)
-//        {
-//            if (ptr1->data > ptr1->next->data)
-//            {
-//                swap(ptr1, ptr1->next);
-//                swapped = 1;
-//            }
-//            ptr1 = ptr1->next;
-//        }
-//        lptr = ptr1;
-//    } while (swapped);
-//}
-//
-///* function to swap data of two nodes a and b*/
-//void swap(struct Node* a, struct Node* b)
-//{
-//    int temp = a->data;
-//    a->data = b->data;
-//    b->data = temp;
+//	return 0;
 //}
