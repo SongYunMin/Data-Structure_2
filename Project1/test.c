1355260f421f6555c2d8238e564957e4bb9bf8b3
Song-YunMin <48168717+SongYunMin@users.noreply.github.com>
2020-09-14 18:08:16 +0900

    Algorithm Update

--- Project1/test.c
+++ Project1/test.c
@@ -1,20 +1,27 @@
-//#include  <stdio.h>
-//
-//
-//void print(int a)
-//{
-//	if (a == 1)
-//		printf("true");
-//	else
-//		printf("false");
-//}
-//int main(void)
-//{
-//	int a = 5, b = 3;
-//
-//	
-//	print(a>b ? 1 : 0);
-//
-//
-//	return 0;
-//}
+#include <stdio.h>
+
+int main(void)
+{
+	int a[5] = { 5,4,2,1,3 };
+	int i, j, temp, count = 0;
+
+	for (i = 0; i < 5; i++) {
+		for (j = 0; j < 5 - 1 - i; j++) {
+			count++;
+			if (a[j] > a[j + 1]) {
+				temp = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = temp;
+			}
+		}
+	}
+
+	printf("%d", count);
+
+	for (i = 0; i < 5; i++) {
+		printf("%d ", a[i]);
+	}
+	
+
+	return 0;
+}
