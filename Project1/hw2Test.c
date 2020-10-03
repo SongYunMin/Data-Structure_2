a0388828a47b6df6e296e7bc1c6de3e0334020e3
YunMin-Song <thd930308@naver.com>
2020-10-03 21:43:56 +0900

    Binary Tree Update

--- Project1/hw2Test.c
+++ Project1/hw2Test.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Data And Link Node
 typedef struct NodeStruct
 {
     int value;
@@ -9,7 +10,8 @@ typedef struct NodeStruct
 }Node;
 
 Node* root = NULL;
- 
+
+// Data Insert Function
 Node* BST_insert(Node* root, int value)
 {
     if (root == NULL)
@@ -29,6 +31,8 @@ Node* BST_insert(Node* root, int value)
     return root;
 }
 
+
+
 Node* findMinNode(Node* root)
 {
     Node* tmp = root;
@@ -36,6 +40,9 @@ Node* findMinNode(Node* root)
         tmp = tmp->leftChild;
     return tmp;
 }
+
+
+// Node Delete Function
 Node* BST_delete(Node* root, int value)
 {
     Node* tNode = NULL;
@@ -63,10 +70,11 @@ Node* BST_delete(Node* root, int value)
         }
 
     }
-
     return root;
 }
 
+
+// Data Search Function
 Node* BST_search(Node* root, int value)
 {
     if (root == NULL)
@@ -91,14 +99,15 @@ void BST_print(Node* root)
 
 int main()
 {
-    root = BST_insert(root, 5);
     root = BST_insert(root, 3);
-    root = BST_insert(root, 7);
+    root = BST_insert(root, 2);
     root = BST_insert(root, 1);
-    root = BST_insert(root, 9);
+    root = BST_insert(root, 4);
+    root = BST_insert(root, 5);
     root = BST_insert(root, 6);
 
-    root = BST_delete(root, 7);
+    // Delete Root
+    root = BST_delete(root, 6);
 
     BST_print(root);
 }
