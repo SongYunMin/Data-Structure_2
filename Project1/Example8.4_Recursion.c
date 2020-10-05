95d111dfe0a6fd9af5d000880a91edf517b8b64a
Song-YunMin <48168717+SongYunMin@users.noreply.github.com>
2020-10-05 17:35:14 +0900

    Level Tree Update

--- Project1/Example8.4_Recursion.c
+++ Project1/Example8.4_Recursion.c
@@ -1,63 +1,63 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <memory.h>
-
-typedef struct TreeNode {
-	int data;
-	struct TreeNode* left, * right;
-} TreeNode;
-
-#define SIZE 100
-int top = -1;
-
-TreeNode* stack[SIZE];
-
-void push(TreeNode* p)
-{
-	if (top < SIZE - 1) {
-		stack[++top] = p;
-	}
-}
-
-TreeNode* pop()
-{
-	TreeNode* p = NULL;
-	if (top >= 0) {
-		p = stack[top--];
-	}
-	return p;
-}
-
-void inorder_iter(TreeNode* root)
-{
-	while (1) {
-		// 왼쪽요소를 쭉 Push
-		for (; root; root = root->left) {
-			push(root);
-		}
-		// 하나씩 꺼내서 출력
-		root = pop();
-		if (!root) break;
-		printf("[%d] ", root->data);
-		// 오른쪽에 값이 있는지 확인(NULL이여도 반복)
-		root = root->right;
-	}
-}
-
-TreeNode n1 = { 1, NULL ,NULL};
-TreeNode n2 = { 4, &n1 ,NULL };
-TreeNode n3 = { 16, NULL ,NULL };
-TreeNode n4 = { 25, NULL ,NULL };
-TreeNode n5 = { 20, &n3, &n4 };
-TreeNode n6 = { 15, &n2 ,&n5 };
-
-TreeNode* root = &n6;
-
-int main(void)
-{
-	printf("중위 순회 = ");
-	inorder_iter(root);
-	printf("\n");
-
-	return 0;
-}
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <memory.h>
+//
+//typedef struct TreeNode {
+//	int data;
+//	struct TreeNode* left, * right;
+//} TreeNode;
+//
+//#define SIZE 100
+//int top = -1;
+//
+//TreeNode* stack[SIZE];
+//
+//void push(TreeNode* p)
+//{
+//	if (top < SIZE - 1) {
+//		stack[++top] = p;
+//	}
+//}
+//
+//TreeNode* pop()
+//{
+//	TreeNode* p = NULL;
+//	if (top >= 0) {
+//		p = stack[top--];
+//	}
+//	return p;
+//}
+//
+//void inorder_iter(TreeNode* root)
+//{
+//	while (1) {
+//		// 왼쪽요소를 쭉 Push
+//		for (; root; root = root->left) {
+//			push(root);
+//		}
+//		// 하나씩 꺼내서 출력
+//		root = pop();
+//		if (!root) break;
+//		printf("[%d] ", root->data);
+//		// 오른쪽에 값이 있는지 확인(NULL이여도 반복)
+//		root = root->right;
+//	}
+//}
+//
+//TreeNode n1 = { 1, NULL ,NULL};
+//TreeNode n2 = { 4, &n1 ,NULL };
+//TreeNode n3 = { 16, NULL ,NULL };
+//TreeNode n4 = { 25, NULL ,NULL };
+//TreeNode n5 = { 20, &n3, &n4 };
+//TreeNode n6 = { 15, &n2 ,&n5 };
+//
+//TreeNode* root = &n6;
+//
+//int main(void)
+//{
+//	printf("중위 순회 = ");
+//	inorder_iter(root);
+//	printf("\n");
+//
+//	return 0;
+//}
--- Project1/Example8.5_Lever_Circuit_Program.c
+++ Project1/Example8.5_Lever_Circuit_Program.c
@@ -57,7 +57,7 @@ void enqueue(QueueType* q, element item)
 element dequeue(QueueType* q)
 {
 	if (is_empty(q)) {
-		error("Queue is Empty");		
+		error("Queue is Empty");
 	}
 	// '%' 원형 Queue
 	q->front = (q->front + 1) % MAX_QUEUE_SIZE;
@@ -65,10 +65,39 @@ element dequeue(QueueType* q)
 	return q->data[q->front];
 }
 
+void level_order(TreeNode* ptr)
+{
+	QueueType q;
 
+	init_queue(&q);
+	if (ptr == NULL) return;
+	enqueue(&q, ptr);
+	while (!is_empty(&q)) {
+		// Root 를 Queue에서 꺼내고 출력함
+		ptr = dequeue(&q);
+		printf(" [%d] ", ptr->data);
+		// Root의 자식 좌우 자식들을 Enqueue 함.
+		if (ptr->left) {
+			enqueue(&q, ptr->left);
+		}
+		if (ptr->right) {
+			enqueue(&q, ptr->right);
+		}
+	}
+}
+
+TreeNode n1 = { 1,NULL,NULL };
+TreeNode n2 = { 4,&n1,NULL };
+TreeNode n3 = { 16,NULL,NULL };
+TreeNode n4 = { 25,NULL,NULL };
+TreeNode n5 = { 20, &n3, &n4 };
+TreeNode n6 = { 15, &n2,&n5 };
+TreeNode* root = &n6;
 
 int main(void)
 {
-
+	printf("레벨 순회");
+	level_order(root);
+	printf("\n");
 	return 0;
 }
