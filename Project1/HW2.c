d96815047eb9c72a90218567eb8627f881696d69
YunMin-Song <thd930308@naver.com>
2020-10-06 23:43:24 +0900

    Update Commit

--- Project1/HW2.c
+++ Project1/HW2.c
@@ -1,28 +1,118 @@
-//// 이진 트리 탐색
-//#include <stdio.h>
-//
-//int main(void)
-//{
-//	FILE* fp;
-//	int arr[10000], count = 0;
-//	fp = fopen("data_2.txt", "rt");
-//
-//	if (fp == NULL) {
-//		printf("File Not Found!!\n");
-//		exit(-1);
-//	}
-//
-//	for (int i = 0; i < 10000; i++) {
-//		fscanf(fp, "%d", &arr[i]);
-//		count++;
-//	}
-//
-//	for (int i = 0; i < 10000; i++) {
-//		printf("%d ", arr[i]);
-//	}
-//	printf("\n\n");
-//
-//	printf("count = %d\n", count);
-//
-//	return 0;
-//}
+// Binary Tree Search Program
+/*
+	Name : Song yun min
+	StuNum : 20174624
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef int element;
+typedef struct TreeNode {
+	element key;
+	struct TreeNode* left, * right;
+}TreeNode;
+
+// Recursive Searching Function
+TreeNode* recursiveSearch(TreeNode* node, int key)
+{
+	if (node == NULL) return NULL;					// node가 비어있으면 종료
+	if (key == node->key) return node;
+	else if (key < node->key)
+		return recursiveSearch(node->left, key);
+	else
+		return recursiveSearch(node->right, key);
+}
+
+// Repetitive Searching Function
+TreeNode* loopSearch(TreeNode* node, int key)
+{
+	while (node != NULL) {
+		if (key == node->key) return node;
+		else if (key < node->key)
+			node = node->left;
+		else
+			node = node->right;
+	}
+	return NULL;		// 탐색에 실패했을 경우
+}
+
+TreeNode* makeNode(int data)
+{
+	TreeNode* tmp = (TreeNode*)malloc(sizeof(TreeNode));
+	tmp->key = data;
+	// Link initialized
+	tmp->left = NULL;
+	tmp->right = NULL;
+	return tmp;
+}
+
+TreeNode* insertLoop(TreeNode* node, int key)
+{
+	// 트리가 공백일 시 새로운 노드 Return
+	if (node == NULL) return makeNode(key);
+
+	// 순환적으로 트리를 내려감
+	if (key < node->key)
+		node->left = insertLoop(node->left, key);
+	else if (key > node->key)
+		node->right = insertLoop(node->right, key);
+
+	return node;
+}
+
+
+
+// Number of Total Nodes
+int getNodeCount(TreeNode* node)
+{
+	int cnt = 0;
+	if (node != NULL) {
+		cnt = 1 + getNodeCount(node->left) + getNodeCount(node->right);
+	}
+
+	return cnt;
+}
+
+// Number of Terminal Nodes
+int getLeafCount(TreeNode* node)
+{
+	int cnt = 0;
+	if (node != NULL) {
+		if (node->left == NULL && node->right == NULL)
+			return 1;
+		else
+			cnt = getLeafCount(node->left) + getLeafCount(node->right);
+	}
+	return cnt;
+}
+
+// Number of Node Height
+int getNodeHeight(TreeNode* node)
+{
+	int height = 0;
+	if (node != NULL) {
+		height = 1 + max(getNodeHeight(node->left), getNodeHeight(node->right));
+	}
+}
+
+int main(void)
+{
+	// File Open And Exception Processing
+	FILE* fp;
+	int data;
+	fp = fopen("data_3.txt", "rt");
+
+	if (fp == NULL) {
+		printf("File Not Found!!\n");
+		exit(-1);
+	}
+
+	// 파일을 읽으며 순환적 탐색 함수 삽입
+	while (!feof(fp)) {
+		fscanf(fp, "%d", &data);
+	}
+
+
+	return 0;
+}
