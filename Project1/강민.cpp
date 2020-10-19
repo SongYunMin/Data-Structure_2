dd4d6f54f247b96d4439cb0e0a57dc90709fbdf8
YunMin-Song <thd930308@naver.com>
2020-10-20 01:53:56 +0900

    Algorithm Update

--- Project1/강민.cpp
+++ Project1/강민.cpp
@@ -1,12 +1,12 @@
-#include <stdio.h>
-
-int main(void)
-{
-	double a;
-
-	a = 0.15 / 0.05;
-
-	printf("%lf", a);
-
-	return 0;
-}
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	double a;
+//
+//	a = 0.15 / 0.05;
+//
+//	printf("%lf", a);
+//
+//	return 0;
+//}
