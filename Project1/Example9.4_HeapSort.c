847be4f861c78119b0e0cc0a7883942393bd2eaa
YunMin-Song <thd930308@naver.com>
2020-10-22 11:32:09 +0900

    Algorithm Update

--- Project1/Example9.4_HeapSort.c
+++ Project1/Example9.4_HeapSort.c
@@ -1,10 +1,11 @@
 //#include <stdio.h>
 //#include <stdlib.h>
-//#define MAX_ELEMENT 200
+//#define MAX_ELEMENT 100
 //
 //typedef struct {
 //	int key;
 //} element;
+//
 //typedef struct {
 //	element heap[MAX_ELEMENT];
 //	int heap_size;
@@ -43,15 +44,12 @@
 //	int parent, child;
 //	element item, temp;
 //
-//	// 1 : 30, 2 : 5
 //	item = h->heap[1];		// 루트 노드 대입
-//	// 1 : 5
 //	temp = h->heap[(h->heap_size)--]; // 마지막 노드 가져옴
-//	parent = 1;		// 1 : 30
-//	child = 2;		// 2 : 10
+//	parent = 1;
+//	child = 2;
 //
 //	while (child <= h->heap_size) {
-//		//h->heap_size--;
 //		// 현재 노드의 자식 노드 중 더 큰 자식노드를 찾음
 //		if ((child < h->heap_size) && (h->heap[child].key < h->heap[child + 1].key))
 //			child++;
@@ -60,36 +58,50 @@
 //		h->heap[parent] = h->heap[child];
 //		parent = child;
 //		child *= 2;
-//		}
+//	}
 //	h->heap[parent] = temp;
 //	return item;
 //}
 //
+//void heap_sort(element a[], int n)
+//{
+//	int i;
+//	HeapType* h;
+//
+//	h = create();
+//	init(h);
+//
+//	for (i = 0; i < n; i++) {
+//		insert_max_heap(h, a[i]);
+//	}
+//	printf("\n\n");
+//
+//	for (i = 0; i < n; i++) {
+//		printf("%d ", a[i]);
+//	}
+//	printf("\n\n");
+//
+//	for (i = 1; i < n+1; i++) {
+//		printf("%d ", h->heap[i]);
+//	}
+//	printf("\n\n");
+//
+//
+//	for (i = (n - 1); i >= 0; i--) {
+//		a[i] = delete_max_heap(h);
+//	}
+//	free(h);
+//}
+//
+//#define SIZE 8
 //int main(void)
 //{
-//	element e1 = { 30 }, e2 = { 10 }, e3 = { 5 };
-//	element e4, e5, e6;
-//	HeapType* heap;
-//
-//	heap = create();
-//	init(heap);
-//
-//	// insert
-//	insert_max_heap(heap, e1);
-//	insert_max_heap(heap, e2);
-//	insert_max_heap(heap, e3);
-//
-//	// Delete
-//	e4 = delete_max_heap(heap);
-//	printf("< %d > ", e4.key);
-//
-//	e5 = delete_max_heap(heap);
-//	printf("< %d > ", e5.key);
-//
-//	e6 = delete_max_heap(heap);
-//	printf("< %d > ", e6.key);
-//
-//	free(heap);
+//	element list[SIZE] = { 23,56,11,9,56,99,27,34 };
+//	heap_sort(list, SIZE);
+//	for (int i = 0; i < SIZE; i++) {
+//		printf("%d ", list[i].key);
+//	}
+//	printf("\n");
 //
 //	return 0;
 //}
