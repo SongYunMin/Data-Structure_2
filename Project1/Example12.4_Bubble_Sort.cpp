cecfc6a8e42cbe07c204207686afd771ae1b34bc
YunMin-Song <thd930308@naver.com>
2020-11-29 22:33:17 +0900

    HW3 Update Commit - Progress

--- Project1/Example12.4_Bubble_Sort.cpp
+++ Project1/Example12.4_Bubble_Sort.cpp
@@ -1,38 +1,38 @@
-#include <iostream>
-
-void bubbleSort(int* list, int n)
-{
-	int i, j, temp;
-
-	// i를 n-1로 하여 정렬된 부분의 반복은 다시 수행하지 않도록 함
-	for (i = n - 1; i > 0; i--) {
-		for (j = 0; j < i; j++) {
-			if (list[j] > list[j + 1]) {
-				temp = list[j];
-				list[j] = list[j + 1];
-				list[j + 1] = temp;
-			}
-		}
-	}
-}
-
-int main(void)
-{
-	int list[6] = { 3,5,8,1,2,7 };
-
-	std::cout << "정렬 되기 전 : " << std::endl;
-	for (int i = 0; i < 6; i++) {
-		std::cout << list[i];
-	}
-	std::cout << std::endl;
-
-	bubbleSort(list, 6);
-
-	std::cout << "정렬 된 이후 : " << std::endl;
-	for (int i = 0; i < 6; i++) {
-		std::cout << list[i];
-	}
-	std::cout << std::endl;
-
-	return 0;
-}
+//#include <iostream>
+//
+//void bubbleSort(int* list, int n)
+//{
+//	int i, j, temp;
+//
+//	// i를 n-1로 하여 정렬된 부분의 반복은 다시 수행하지 않도록 함
+//	for (i = n - 1; i > 0; i--) {
+//		for (j = 0; j < i; j++) {
+//			if (list[j] > list[j + 1]) {
+//				temp = list[j];
+//				list[j] = list[j + 1];
+//				list[j + 1] = temp;
+//			}
+//		}
+//	}
+//}
+//
+//int main(void)
+//{
+//	int list[6] = { 3,5,8,1,2,7 };
+//
+//	std::cout << "정렬 되기 전 : " << std::endl;
+//	for (int i = 0; i < 6; i++) {
+//		std::cout << list[i];
+//	}
+//	std::cout << std::endl;
+//
+//	bubbleSort(list, 6);
+//
+//	std::cout << "정렬 된 이후 : " << std::endl;
+//	for (int i = 0; i < 6; i++) {
+//		std::cout << list[i];
+//	}
+//	std::cout << std::endl;
+//
+//	return 0;
+//}
--- Project1/HW3.c
+++ Project1/HW3.c
@@ -1,12 +1,84 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+
+clock_t start, finish;
+double timeResult;
+
+void printArray(char** arr, int n) {
+	for (int i = 0; i < n; i++) {
+		printf("%s\n", arr[i]);
+	}
+	printf("--------------------------------\n\n");
+}
+
+void bubbleSort(char** arr, int n) {
+	int i, j, temp;
+	start = clock();
+	for (i = n - 1; i > 0; i--) {
+		for (j = 0; j < i; j++) {
+			if (strcmp(arr[j], arr[j + 1]) == 1) {
+				temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+	finish = clock();
+	timeResult = (double)(finish - start) / CLOCKS_PER_SEC;
+
+	printf("----------Bubble Sort-----------\n");
+	printf("실행 시간 : %lf\n", timeResult);
+	printArray(arr, n);
+}
+
+void insertionSort(char** arr, int n) {
+	int i, j;
+	char* key;
+
+	start = clock();
+	for (i = 1; i < n; i++) {
+		key = arr[i];
+		for (j = i - 1; j >= 0 && strcmp(arr[j], key) == 1; j--) {
+			arr[j + 1] = arr[j];
+		}
+		arr[j + 1] = key;
+	}
+	finish = clock();
+	timeResult = (double)(finish - start) / CLOCKS_PER_SEC;
+	printf("---------Insertion Sort---------\n");
+	printf("실행 시간 : %lf\n", timeResult);
+	printArray(arr, n);
+}
+
+void SelectionSort(char** arr, int n) {
+	int i, j, least, temp;
+	for (i = 0; i < n - 1; i++) {
+		
+	}
+	printArray(arr, n);
+}
+
+void shellSort(char** arr, int n) {
+	printArray(arr, n);
+}
+
+void mergeSort(char** arr, int n) {
+	printArray(arr, n);
+}
+
+void quickSort(char** arr, int n) {
+	printArray(arr, n);
+}
 
 int main(void)
 {
-	int count = 0;
+	int count = 0, i, len, status = 0;
+	char** stringArr;
+	char buf[100];
 	FILE* fp;
-	fp = fopen("data.txt", "rt");
+	fp = fopen("text.txt", "rt");
 
 	// 예외 처리
 	while (fp == NULL) {
@@ -14,11 +86,31 @@ int main(void)
 		return -1;
 	}
 
+	// 배열 입력받음
 	while (!feof(fp)) {
-		
+		fscanf(fp, "%s", buf);
+
+		char* ptr = (char*)malloc(sizeof(char) * strlen(buf + 1));
+		strcpy(ptr, buf);
+
+		// 처음 입력 받을땐 malloc으로 초기화
+		if (status == 0) {
+			stringArr = (char*)malloc(sizeof(char) * strlen(buf + 1) * 2);
+			stringArr[count++] = ptr;
+			status++;
+		}
+		// 처음 이후 부터는 realloc으로 메모리 공간 재할당
+		else {
+			stringArr = (char*)realloc(stringArr, sizeof(char) *
+				(strlen(stringArr) + strlen(buf + 1)) * 2);
+			stringArr[count++] = ptr;
+		}
 	}
-
-
+	
+	printf("---------Default Array----------\n");
+	printArray(stringArr, count);
+	bubbleSort(stringArr, count);
+	insertionSort(stringArr, count);
 
 	return 0;
 }
