162e275004782fac45a23b14394c3806200a17d9
YunMin-Song <thd930308@naver.com>
2020-12-08 03:27:19 +0900

    Algorithm Update

--- Project1/RSA.cpp
+++ Project1/RSA.cpp
@@ -1,15 +1,15 @@
-#include <iostream>
-#include <vector>
-
-int main(void)
-{
-	int p, q, n;
-	std::cout << "소수 p, 소수 q 를 입력하세요." << std::endl;
-	std::cin >> p >> q;
-	
-	n = p * q;
-	std::cout << "n값은 : " << n << " 입니다.";
-	
-
-	return 0;
-}
+//#include <iostream>
+//#include <vector>
+//
+//int main(void)
+//{
+//	int p, q, n;
+//	std::cout << "소수 p, 소수 q 를 입력하세요." << std::endl;
+//	std::cin >> p >> q;
+//	
+//	n = p * q;
+//	std::cout << "n값은 : " << n << " 입니다.";
+//	
+//
+//	return 0;
+//}
